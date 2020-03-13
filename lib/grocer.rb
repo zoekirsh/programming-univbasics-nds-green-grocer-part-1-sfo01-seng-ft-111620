53dc360fb9ae320a8b5eb2727e8793884d384453
Maxwell Benton <maxwellbenton@gmail.com>
2020-03-13 16:41:21 -0400

    add alt solutions

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,6 +7,17 @@ def find_item_by_name_in_collection(name, collection)
   end
 end
 
+## Alternative Solution with Enumerables
+
+# def find_item_by_name_in_collection(name, collection)
+#   index = 0
+#
+#   collection.each do |grocery_item|
+#     return grocery_item if grocery_item[:item] === name 
+#     index += 1
+#   end
+# end
+
 def consolidate_cart(cart)
   index = 0
   new_cart = []
@@ -30,5 +41,28 @@ def consolidate_cart(cart)
   new_cart
 end
 
+## Alternative Solution with Enumerables
 
+# def consolidate_cart(cart)
+#   index = 0
+#   new_cart = []
+#  
+#   cart.each do |grocery_item|
+#     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+#     if current_item
+#       new_cart_index = 0
+#       new_cart.each do |new_cart_item|
+#         if new_cart_item[:item] === current_item[:item]
+#           new_cart_item[:count] += 1
+#         end
+#         new_cart_index += 1
+#       end
+#     else
+#       grocery_item[:count] = 1
+#       new_cart << grocery_item
+#     end
+#     index += 1
+#   end
+#   new_cart
+# aend
   
