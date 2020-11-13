6656e92a3d3d5c4fd20a4a20ea154ede5b462b20
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-13 23:50:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,7 @@ def consolidate_cart(cart)
    scanned_cart = []
    cart.each do |itemHash|
      if find_item_by_name_in_collection(itemHash[:item], scanned_cart)
-     
+      
      
      scanned_cart << item[:count] = 1
       
