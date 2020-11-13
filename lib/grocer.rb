23ea43c9403a5bf3d01748aa0b5f590c3b53dbcb
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-13 23:30:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -12,8 +12,9 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  # REMEMBER: This returns a new Array that represents the cart.
+  cart_total = [];
+  
   
   
 end
