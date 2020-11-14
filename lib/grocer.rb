0dca3244ea1becc0ebe54ca106d5ea90bb9ee10b
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-14 00:21:07 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,12 +3,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   # Iterates thru collection and returns the full hash for an item matching name. 
   #If no match, returns nil 
-  collection.each do |itemHash|
-   if itemHash[:item] == name 
-     return itemHash
-    end
-  end
-  return nil
+  collection.find 
 end
 
 def consolidate_cart(cart)
